63c9269d7648b580dd7858ae8fe9377045652cb6
The-Napster <connorh16@gmail.com>
2020-05-09 15:09:59 -0400

    Changed the code to be more simple.

---
+++ assets/javascript/app.js
@@ -0,0 +1,134 @@
+$(document).ready(function () {
+    var startButton = $("#startButton");
+
+    var triviaGame = $("#triviaGame");
+
+    var formAnswers = $("#formAnswers");
+
+    var formInput = $("#input")
+
+    var input0 = "<input type='radio' class='answer' value='true' name='userSelect0'>";
+
+    var input1 = "<input type='radio' class='answer' value='true' name='userSelect1'>";
+
+    var input2 = "<input type='radio' class='answer' value='true' name='userSelect2'>";
+
+    var input3 = "<input type='radio' class='answer' value='true' name='userSelect3'>";
+
+    var incorrectInput0 = "<input type='radio' class='answer' value='false' name='userSelect0'>";
+
+    var incorrectInput1 = "<input type='radio' class='answer' value='false' name='userSelect1'>";
+
+    var incorrectInput2 = "<input type='radio' class='answer' value='false' name='userSelect2'>";
+
+    var incorrectInput3 = "<input type='radio' class='answer' value='false' name='userSelect3'>";
+
+
+    var timer;
+
+    var timerRunning = false;
+
+    // example
+
+    var myQuestions = [
+        {
+            question: "<p>What was the first full length CGI movie?</p>",
+            answers: [
+                "Toy Story",
+                "Cars",
+                "Monsters, Inc.",
+                "Finding Nemo",
+            ],
+            correctAnswer: 0,
+        },
+        {
+            question: "<p>Which of these is NOT a name of one of the Spice Girls?</p>",
+            answers: [
+                "Emma Bunton",
+                "Geri Halliwell",
+                "Victoria Beckham",
+                "Hayley Williams",
+            ],
+            correctAnswer: 3,
+        },
+        {
+            question: "<p>Which NBA team won the most titles in the 90s?</p>",
+            answers: [
+                "Bulls",
+                "Spurs",
+                "Lakers",
+                "Celtics",
+            ],
+            correctAnswer: 1,
+        },
+        {
+            question: "<p>Where was the first computer built?</p>",
+            answers: [
+                "University of Pennsylvania",
+                "Princeton University",
+                "Brown University",
+                "Yale University",
+            ],
+            correctAnswer: 0,
+        },
+
+    ]
+
+    triviaGame.hide();
+
+    startButton.on("click", function () {
+        triviaGame.show();
+        startButton.hide();
+        // if (!timerRunning) {
+        //     timer = setInterval(count, 1000)
+        //     timerRunning = true;
+        // };
+    });
+
+
+
+    function startGame() {
+        formAnswers.append(myQuestions[0].question);
+        formAnswers.append(input0 + myQuestions[0].answers[0]);
+        formAnswers.append(incorrectInput0 + myQuestions[0].answers[1]);
+        formAnswers.append(incorrectInput0 + myQuestions[0].answers[2]);
+        formAnswers.append(incorrectInput0 + myQuestions[0].answers[3]);
+
+        formAnswers.append(myQuestions[1].question);
+        formAnswers.append(incorrectInput1 + myQuestions[1].answers[0]);
+        formAnswers.append(incorrectInput1 + myQuestions[1].answers[1]);
+        formAnswers.append(incorrectInput1 + myQuestions[1].answers[2]);
+        formAnswers.append(input1 + myQuestions[1].answers[3]);
+
+        formAnswers.append(myQuestions[2].question);
+        formAnswers.append(incorrectInput2 + myQuestions[2].answers[0]);
+        formAnswers.append(input2 + myQuestions[2].answers[1]);
+        formAnswers.append(incorrectInput2 + myQuestions[2].answers[2]);
+        formAnswers.append(incorrectInput2 + myQuestions[2].answers[3]);
+
+        formAnswers.append(myQuestions[3].question);
+        formAnswers.append(input3 + myQuestions[3].answers[0]);
+        formAnswers.append(incorrectInput3 + myQuestions[3].answers[1]);
+        formAnswers.append(incorrectInput3 + myQuestions[3].answers[2]);
+        formAnswers.append(incorrectInput3 + myQuestions[3].answers[3]);
+
+        triviaGame.append("<button type='button'> submit </button>");
+    };
+
+    // function getInputValue() {
+    //     if(correctAnswer.value === true){
+    //         console.log("it works !");
+    //     }else{
+    //         console.log("123");
+    //     };
+    // };
+
+    // function endGame() {
+    //     clearInterval(intervalId);
+    //     timerRunning = false;
+    // }
+
+    startGame();
+    
+    // getInputValue();
+});
